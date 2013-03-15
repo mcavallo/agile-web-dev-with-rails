6254cf94ae3110effbd78a0a48a79e5a9020d8e5
Mariano Cavallo <mariano.cavallo@gmail.com>
2013-03-15 16:59:53 -0300

    Added validation to Products model. Played with the tests. Tweaked form styles.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,31 @@
 class Product < ActiveRecord::Base
+
+	validates :title,
+		:presence => true,
+		:uniqueness => true,
+		:length => {
+			:minimum => 5,
+			:maximum => 150
+		}
+
+	validates :description,
+		:presence => true,
+		:length => {
+			:minimum => 50
+		}
+
+	validates :image_url,
+		:presence => true,
+		:format => {
+			:with => %r{\.(gif|jpg|png)$}i,
+			:message => 'must be a gif, jpg or png image.'
+		}
+
+	validates :price,
+		:numericality => {
+			:greater_than_or_equal_to => 0.00
+		}
+
   attr_accessible :description, :image_url, :price, :title
+
 end
