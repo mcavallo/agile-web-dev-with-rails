597bf4efc162103618adc13f8408d6ce2ec9b8d1
Mariano Cavallo <mariano.cavallo@gmail.com>
2013-03-16 00:20:29 -0300

    Added Cart and LineItem. Added cart markup, styles, remove item and empty cart. Added quantity field to line_items. Several style tweaks.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ActiveRecord::Base
 
 	default_scope :order => 'title'
 
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title,
 		:presence => true,
 		:uniqueness => true,
@@ -29,5 +32,16 @@ class Product < ActiveRecord::Base
 		}
 
   attr_accessible :description, :image_url, :price, :title
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+  	if line_items.empty?
+  		return true
+  	else
+  		errors.add(:base, 'Line Items Present')
+  		return false
+  	end
+  end
 
 end
